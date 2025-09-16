f975e0f84c48ad89a9bb7fc25e1a5617a9f64a3e
Jonathan <jbrow365@jh.edu>
2025-09-16 13:27:00 -0400

    Cleaned up the formatting of the output dataframe so row indices are reset and entries sorted by Hospital.  Option for exporting to CSV

--- Main.py
+++ Main.py
@@ -1,9 +1,12 @@
 import Hungarian_Alg_Steps as HAS
 import Hungarian_Alg_LineCheck as HAL
 import HungarianAlgoImportData as HAID
+from datetime import datetime as dt
 
-df = HAID.import_data('Large_Test_500x50.csv')
-hi = HAID.get_hospital_info(df)
+start = dt.now() # Start timer
+
+
+df = HAID.import_data('Large_Test_500x50.csv') # A csv file in format from README.md
 prepped_df = HAID.prep_data(df)
 prepped_array = prepped_df.to_numpy(copy = True)
 
@@ -19,12 +22,22 @@ while len(row_lines) + len(col_lines) != len(reduced_matrix):
 assignments = HAL.STEP5_find_solution(marked_zeros)
 num_to_match = len(HAID.get_doctors(df))
 matches_df = HAL.match_residents(assignments, df)
-matches_df = matches_df.sort_values("Doctor")
+
+# How much time has elapsed to end of actual algorithm run
+end = dt.now()
+
+# Dropping sorting by hospital and dropping the row indices for style
+matches_df = matches_df.sort_values("Hospital Position")
+matches_df = matches_df.reset_index(drop=True)
+
+
 
 matches_numeric = HAL.match_residents_numeric(assignments, prepped_df, num_to_match)
 score, max_score = HAID.get_score(prepped_df, matches_numeric)
 print(matches_df)
-print(f'\nWe have achieved a solution of: {(1 - score / max_score) * 100:.1f}%')
+print(f'\nWe have achieved a solution of: {(1 - score / max_score) * 100:.1f}%\n which took {end-start}')
+
+#matches_df.to_csv("Test_Result.csv") # Uncomment this if you'd like the output in csv format
 
 
 
